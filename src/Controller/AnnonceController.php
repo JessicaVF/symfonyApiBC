8c5c51086a8fd832a709fdb63b9a9cdcfc373cf3
JessicaVF <villarjessica@hotmail.com>
2021-09-02 16:35:03 +0200

    Add annonce api format

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Annonce;
+use App\Entity\FuelType;
 use App\Repository\AnnonceRepository;
+use App\Repository\FuelTypeRepository;
 use App\Repository\GarageRepository;
 use App\Repository\MakeRepository;
 use App\Repository\ModelRepository;
@@ -37,21 +39,26 @@ class AnnonceController extends AbstractController
     /**
      * @Route("/annonce/create", name="createAnnonce", methods={"POST"})
      */
-    public function create(GarageRepository $garageRepository, UserRepository $userRepository, MakeRepository $makeRepository, ModelRepository $modelRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $manager):Response
+    public function create(GarageRepository $garageRepository, UserRepository $userRepository, MakeRepository $makeRepository, ModelRepository $modelRepository, FuelTypeRepository $fuelTypeRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $manager):Response
     {
 
-        $annonce = $serializer->deserialize($request->getContent(), Annonce::class, 'json');
+        $data = $request->toArray();
+        $dataToRetrieve= $data[0];
 
-        $make = $makeRepository->find('3');
-        $model = $modelRepository->find('3');
-        $user = $userRepository->find('2');
-        $garage = $garageRepository->find('9');
+        $make = $makeRepository->find($dataToRetrieve['make']);
+        $model = $modelRepository->find($dataToRetrieve['model']);
+        $user = $userRepository->find($dataToRetrieve['user']);
+        $garage = $garageRepository->find($dataToRetrieve['garage']);
+        $fuelType = $fuelTypeRepository->find($dataToRetrieve['fuelType']);
+
+        $annonceInfo = $serializer->serialize($data[1], 'json');
+        $annonce = $serializer->deserialize($annonceInfo, Annonce::class, 'json');
 
         $annonce->setMake($make);
         $annonce->setModel($model);
         $annonce->setAuthor($user);
         $annonce->setGarage($garage);
-
+        $annonce->setfuelType($fuelType);
 
         $manager->persist($annonce);
         $manager->flush();
